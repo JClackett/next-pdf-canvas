baed709f46c95968b188869461b32c1159bbeb3b
Jack Clackett <jack@noquarter.co>
2024-09-16 13:24:34 +0200

    tweak examples

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@react-pdf/renderer", "pdfjs-dist"],
+    serverComponentsExternalPackages: ["pdfjs-dist"],
   },
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import dynamic from "next/dynamic"
+import "@react-pdf-viewer/core/lib/styles/index.css"
+import "@react-pdf-viewer/default-layout/lib/styles/index.css"
+import "@react-pdf-viewer/toolbar/lib/styles/index.css"
 
 export const PdfViewerContainer = dynamic(() => import("./PdfViewer"), {
   ssr: false,
@@ -6,9 +9,9 @@ export const PdfViewerContainer = dynamic(() => import("./PdfViewer"), {
 
 export default function Page() {
   return (
-    <div>
-      <h1>Test pdf</h1>
-      <PdfViewerContainer url="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" />
+    <div className="max-w-5xl mx-auto pt-10 space-y-4">
+      <h1 className="text-4xl font-bold">Test pdf</h1>
+      <PdfViewerContainer url="https://pdfobject.com/pdf/sample.pdf" />
     </div>
   )
 }
